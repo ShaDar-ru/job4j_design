5151149f43b81722fef035d795ad1c045cb8547d
ShaDar <alexterentev90@gmail.com>
2021-08-29 12:41:58 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -9,14 +9,19 @@ import java.io.File;
  */
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("/home/alex/projects");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.printf("Name : %s%n", file.getName());
-        System.out.printf("Size : %s%n", file.length());
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.getName());
+        }
     }
 }
--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,14 +8,39 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
-    public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("txt")).forEach(System.out::println);
+
+    public static void validate(String[] args) throws IllegalArgumentException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException(
+                    "Root folder is null. Usage java -jar search.jar <ROOT_FOLDER> <SEARCHED_FILE_TYPE>.");
+        }
+        Path start = Paths.get(args[0]);
+        if (!start.toFile().isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", start.toAbsolutePath().toString()));
+        }
+        if (args.length < 2) {
+            throw new IllegalArgumentException(
+                    "Searched type file  is null. Usage java -jar search.jar <ROOT_FOLDER> <SEARCHED_FILE_TYPE>.");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPath();
+    }
+
+    public static void searchind(String[] args) throws IOException {
+        Path start = Paths.get(args[0]);
+        String searchedFileType = args[1];
+        search(start, p -> p.toFile()
+                .getName()
+                .endsWith(searchedFileType))
+                .forEach(System.out::println);
+    }
+
+    public static void main(String[] args) throws IOException {
+        validate(args);
+        searchind(args);
     }
 }

c78bea8a1c54e5aeb45d52df4842180ed462398f
ShaDar <alexterentev90@gmail.com>
2021-09-22 22:56:37 +0300

    3. Фильтры [#1732] fix LineLength

--- resources/products/get data.sql
+++ resources/products/get data.sql
@@ -1,9 +1,13 @@
 select * from product where type_id = 1; 
 --аналогично первому, если мы не знаем ID сыра
-select * from product inner join type on type_id = type.id where type.name ilike '%сыр%'; 
+select * from product inner join type on type_id = type.id
+where type.name ilike '%сыр%';
 select * from product where name ilike '%мороженое%';
 select * from product where expired_date < current_date;
-select type.name, count(type.id) from type inner join product on type.id = product.type_id group by type.name;
+select type.name, count(type.id) from type inner join product
+on type.id = product.type_id group by type.name;
 select name, price from product where price = (select max(price) from product);
-select * from product inner join type on type_id=type.id where type.name ilike '%сыр%' or type.name ilike '%молоко%';
-select type.name, count(type_id) from product inner join type on type_id=type.id group by type.name having count(type_id) < 10;
+select * from product inner join type on type_id=type.id where type.name
+ilike '%сыр%' or type.name ilike '%молоко%';
+select type.name, count(type_id) from product inner join type on type_id=type.id
+ group by type.name having count(type_id) < 10;
